0f47b3e285573ceeafdf287fc3813731c69789af
Bumindu Yasith <bumiduyasith@gmail.com>
2025-03-03 21:42:48 +0530

    Add sample implementation of React agent with weather tool and graph structure

---
+++ hitl_sample_2.py
@@ -0,0 +1,80 @@
+from langgraph.prebuilt import create_react_agent
+from langgraph.checkpoint.memory import MemorySaver
+from langchain_core.tools import tool
+from typing import Literal
+import os
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+
+# For this tutorial we will use custom tool that returns pre-defined values for weather in two cities (NYC & SF)
+
+
+@tool
+def get_weather(location: str):
+    """Use this to get weather information from a given location."""
+    if location.lower() in ["nyc", "new york"]:
+        return "It might be cloudy in nyc"
+    elif location.lower() in ["sf", "san francisco"]:
+        return "It's always sunny in sf"
+    else:
+        raise AssertionError("Unknown Location")
+
+
+@tool
+def multiply(num1: int, num2: int) -> int:
+    """
+    Multiplies two integers and returns the product.
+
+    Args:
+        num1 (int): The first number.
+        num2 (int): The second number.
+
+    Returns:
+        int: The product of num1 and num2.
+    """
+
+    return num1 * num2
+
+
+tools = [get_weather, multiply]
+
+# We need a checkpointer to enable human-in-the-loop patterns
+
+memory = MemorySaver()
+
+# Define the graph
+
+# graph = create_react_agent(
+#     model, tools=tools, interrupt_before=["tools"], checkpointer=memory
+# )
+
+graph = create_react_agent(
+    model, tools=tools, interrupt_before=['get_weather'], checkpointer=memory
+)
+
+
+def print_stream(stream):
+    """A utility to pretty print the stream."""
+    for s in stream:
+        message = s["messages"][-1]
+        if isinstance(message, tuple):
+            print(message)
+        else:
+            message.pretty_print()
+
+
+config = {"configurable": {"thread_id": "42"}}
+# inputs = {"messages": [("user", "what is the weather in new york")]}
+inputs = {"messages": [("user", "what 5*6")]}
+
+print_stream(graph.stream(inputs, config, stream_mode="values"))
+
+answer = input(">>> ")
+
+print_stream(graph.stream(None, config, stream_mode="values"))
---
+++ hitl_sample_3.py
@@ -0,0 +1,89 @@
+from langgraph.prebuilt import create_react_agent
+from langgraph.checkpoint.memory import MemorySaver
+from langchain_core.tools import tool
+from typing import Literal
+import os
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langgraph.prebuilt.chat_agent_executor import (
+    AgentState,
+)
+from langgraph.graph import START, END, Graph, MessagesState
+from langgraph.errors import GraphInterrupt
+from langgraph.types import Command
+load_dotenv()
+
+model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+memory = MemorySaver()
+
+
+class AgentState(MessagesState):
+    """State for our agents."""
+    pass
+
+
+@tool
+def get_weather(location: str):
+    """Use this to get weather information from a given location."""
+    print("get_weather")
+    if location.lower() in ["nyc", "new york"]:
+        return "It might be cloudy in nyc"
+    elif location.lower() in ["sf", "san francisco"]:
+        return "It's always sunny in sf"
+    else:
+        raise AssertionError("Unknown Location")
+
+
+tools = [get_weather]
+
+react_agent = create_react_agent(
+    model, tools=tools, interrupt_before=["tools"], checkpointer=memory
+)
+
+
+def finalized(agent_state: AgentState):
+    """A function to handle finalization."""
+    print("Finalized")
+    print(agent_state)
+
+
+builder = Graph()
+builder.add_node("react_agent", react_agent)
+builder.add_node("finalized", finalized)
+
+builder.add_edge("react_agent", "finalized")
+builder.add_edge("finalized", END)
+
+builder.set_entry_point("react_agent")
+
+graph = builder.compile(checkpointer=memory)
+
+config = {"configurable": {"thread_id": "42"}}
+inputs = {"messages": [("user", "what is the weather in new york")]}
+
+# result = graph.invoke({"messages": [
+#     HumanMessage(
+#         content="what is the weather in new york")
+# ]}, config=config)
+
+
+def print_stream(stream):
+    """A utility to pretty print the stream."""
+    for s in stream:
+        if "messages" not in s:
+            print(s)
+            continue
+        message = s["messages"][-1]
+        if isinstance(message, tuple):
+            print(message)
+        else:
+            message.pretty_print()
+
+
+print_stream(graph.stream(inputs, config, stream_mode="values"))
+
+answer = input(">>> ")
+
+print_stream(graph.stream(Command(resume=answer),
+             config, stream_mode="values"))
