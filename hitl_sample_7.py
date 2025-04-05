1f16c80297f9590e8490259a70c4d8c8395ba848
bumindu <bumiduyasith@gmail.com>
2025-04-05 18:32:15 +0530

    checking

--- hitl_sample_7.py
+++ hitl_sample_7.py
@@ -43,10 +43,11 @@ ag = create_react_agent(
         """you are a helpful assistant. greet user and reply to user question friendly. 
         if user ask to get weather information, use the tools you have.""")
 )
+
+
 def get_agent(state):
     result = ag.invoke({"messages": state["messages"]})
     return result
-    
 
 
 def finalized(agent_state: AgentState):
@@ -64,7 +65,6 @@ builder.set_entry_point("get_agent")
 config = {"configurable": {"thread_id": "44"}}
 
 
-
 def print_stream(stream):
     """A utility to pretty print the stream."""
     for s in stream:
@@ -77,18 +77,12 @@ def print_stream(stream):
 DB_URI = "postgresql://postgres:mysecretpassword@localhost:5432/langgraph_db?sslmode=disable"
 
 
-def start_conversation(msg: str, thread_id=None):
-    """Start a new conversation.
+def start_conversation(thread_id=None):
 
-    Args:
-        thread_id: Optional thread ID to use for the conversation.
-    """
-    # Use the provided thread_id if available, otherwise use the default
     conversation_config = config.copy()
     if thread_id:
         conversation_config["configurable"]["thread_id"] = thread_id
-        
-    inputs = {"messages": [    HumanMessage( content=msg)]}
+
     with PostgresSaver.from_conn_string(DB_URI) as postgres_checkpointer:
         connection_kwargs = {
             "autocommit": True,
@@ -97,77 +91,21 @@ def start_conversation(msg: str, thread_id=None):
 
         postgres_checkpointer.setup()
         graph = builder.compile(checkpointer=postgres_checkpointer)
+        msg = graph.get_state(conversation_config).values
         print(
             f"Starting a new conversation with thread_id: {conversation_config['configurable']['thread_id']}...")
-        for s in graph.stream(
-            inputs, conversation_config, stream_mode="values", subgraphs=True):
-            data = s[-1]
-            messages = data.get('messages')
-            if messages and isinstance(messages[-1], AIMessage):  # Check if message is an instance of HumanMessage
+        
+        while True:
+            msg = input("Enter your message: ")
+            inputs = {"messages": [HumanMessage(content=msg)]}
+            for s in graph.stream(
+                    inputs, conversation_config, stream_mode="values", subgraphs=True):
+                data = s[-1]
+                messages = data.get('messages')
+                # Check if message is an instance of HumanMessage
+                if messages and isinstance(messages[-1], AIMessage):
                     print(messages[-1].content)
 
 
-
-
-# answer = input(">>> ")
-
-# print_stream(graph.stream(Command(resume=answer),
-#              config, stream_mode="values"))
-
-
-def resum_graph(thread_id=None):
-    """Resume a conversation.
-
-    Args:
-        thread_id: Optional thread ID to use for the conversation.
-    """
-    # Use the provided thread_id if available, otherwise use the default
-    conversation_config = config.copy()
-    if thread_id:
-        conversation_config["configurable"]["thread_id"] = thread_id
-
-    DB_URI = "postgresql://postgres:mysecretpassword@localhost:5432/langgraph_db?sslmode=disable"
-    with PostgresSaver.from_conn_string(DB_URI) as postgres_checkpointer:
-        connection_kwargs = {
-            "autocommit": True,
-            "prepare_threshold": 0,
-        }
-
-        postgres_checkpointer.setup()
-        graph = builder.compile(checkpointer=postgres_checkpointer)
-        state = graph.get_state(conversation_config).values
-        next =graph.get_state(conversation_config).next
-        print(
-            f"Resuming conversation with thread_id: {conversation_config['configurable']['thread_id']}...")
-        print_stream(graph.stream(Command(resume="yes"),
-                                  conversation_config, stream_mode="values"))
-
-
-# poetry run python .\hitl_sample_6.py sc 10 (10 -> thread_id)
-# poetry run python .\hitl_sample_6.py rs 10 (10 -> thread_id)
-
-# if __name__ == "__main__":
-#     import sys
-
-#     if len(sys.argv) > 1:
-#         command = sys.argv[1]
-#         # Check if a thread_id is provided as the third argument
-#         thread_id = sys.argv[2] if len(sys.argv) > 2 else None
-
-#         if command == "sc":
-#             start_conversation(thread_id)
-#         elif command == "rs":
-#             resum_graph(thread_id)
-#         else:
-#             print("Usage: python hitl_sample_6.py [sc|rs] [thread_id]")
-#     else:
-#         start_conversation(thread_id)
-#         print("Usage: python hitl_sample_6.py [sc|rs] [thread_id]")
-
-while True:
-    thread_id = "003"
-    command = input("Enter your message: ")
-    if command != "yes":
-        start_conversation(command, thread_id)
-    else:
-        resum_graph(thread_id)
+thread_id = "005"
+start_conversation(thread_id)
